39416b5495bfd19e098fddfb2f4f554518a3c362
star8088 <star8088@users.noreply.github.com>
2023-08-23 05:59:32 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bb56afe4-7858-439f-a2bf-b5b0cf355dcc';
 
-let proxyIP = '';
+let proxyIP = 'cdn.anycast.eu.org ';
 
 
 if (!isValidUUID(userID)) {
